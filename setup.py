842c3153a3ff806616d6cfa16e87513b3cf49e16
vangheem <vangheem@gmail.com>
2017-04-24 07:51:07 -0500

    some metadata

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from setuptools import setup
 setup(
     name='guillotina_gcloudstorage',
     version=open('VERSION').read().strip(),
+    description='guillotina gcloud storage support',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGELOG.rst').read()),
     classifiers=[
-        'Framework :: Plone :: 7.0',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    author='Ramon Navarro Bosch',
+    author_email='ramon@plone.org',
     url='https://pypi.python.org/pypi/guillotina_gcloudstorage',
     license='GPL version 3',
     setup_requires=[
